bd9ccc15233235212577a649fcbf409ba608baca
Nekitisov <88847384+Nekitisov@users.noreply.github.com>
2023-09-24 22:17:29 +0300

    add

---
+++ Learning/color.js
@@ -0,0 +1,20 @@
+// let x = 0;
+// while (true) {
+//     let b = +prompt('fgh', '');
+
+//     if (!b) {
+//         break;
+// //     }
+
+// //     x += b;
+// // }
+// // alert (x);
+// "use strict";
+
+// // let x = +prompt('');
+// // if (x = 5) {
+// //     alert ('wow');
+// // } else if (x == null);
+
+// document.body.style.background = 'red';
+// setTimeout(() => document.body.style.background = '', 1000)
