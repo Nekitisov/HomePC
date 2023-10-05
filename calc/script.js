5889b873b5f2c1741d7d9884c4a003d3e538ce9c
Nekitisov <88847384+Nekitisov@users.noreply.github.com>
2023-10-05 17:09:55 +0300

    v 0.7

--- calc/script.js
+++ calc/script.js
@@ -1,4 +1,24 @@
-const num = document.querySelector('.btn:not(.btnEquals)'),
-      result = document.querySelector('#result')
+let number = document.querySelectorAll('.btn'),
+    equals = document.querySelector('.btnEquals'),
+    conclusion = document.querySelector('#result'),
+    clear = document.querySelector('#ce');
 
 
+number.forEach(function(id) {
+    id.addEventListener ('click', function() {
+        conclusion.innerHTML += this.id;
+    })
+});
+
+equals.addEventListener('click', function() {
+    let text = conclusion.textContent.split(' ');
+    if (text[1] === '+') {
+        conclusion.innerHTML = +text[0] + +text[2];
+    } else if (text[1] === '-') {
+        conclusion.innerHTML = +text[0] - +text[2]
+    } else if (text[1] === '*') {
+        conclusion.innerHTML = +text[0] * +text[2]
+    } else if (text[1] === '/') {
+        conclusion.innerHTML = +text[0] / +text[2]
+    }
+})
